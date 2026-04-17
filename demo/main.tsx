4c071d9a278fd1fbae0e90ff0feb71b588486ed2
ngtrio <ngtrio@protonmail.com>
2026-04-17 06:28:58 +0000

    Add demo markdown feature support

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,9 +1,10 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {RouterProvider} from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
 
+import 'katex/dist/katex.min.css'
 import './styles.css'
-import {router} from './router.js'
+import { router } from './router.js'
 
 const container = document.getElementById('app')
 
